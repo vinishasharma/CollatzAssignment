06e3b4baa98b8afc3fee78563c422883d2ab7055
vinishasharma <vinisha.sharma@knoldus.in>
2018-01-16 00:08:00 +0530

    solving collatz using list

--- src/main/scala/edu/knoldus/Operation.scala
+++ src/main/scala/edu/knoldus/Operation.scala
@@ -1,31 +1,29 @@
 package edu.knoldus
 
-import scala.collection.mutable.ListBuffer
-
 class Operation {
 
-  def collatz(number: Int, map : Map[Int,List[Int]]): List[Int] = {
-    val list = new ListBuffer[Int]
-    list += number
-
-    def f1(number: Int): List[Int] = {
+  def collatz(number: Int,list: List[Int]): List[Int] = {
       number match {
-        case n if n == 1 => list.toList
-        case n => if (n % 2 == 0) findKey(n / 2, map) else findKey(3 * number + 1, map)
-      }
+        case n if n == 1 => list
+        case n => if(n % 2 == 0){
+          val num: Int = n/2
+          val existResult = findNumInList(num, list)
+          if (existResult) list else collatz(num, num :: list)
+          }
+          else {
+          val num: Int = 3 * n + 1
+          val existResult = findNumInList(num, list)
+          if (existResult) list else collatz(num, num :: list)
+          }
     }
+  }
 
-    def findKey(number: Int, map: Map[Int, List[Int]]): List[Int] = {
-      if (map.exists(x => x._1 == number)) {
-        list.toList ::: map(number)
-
-      }
-      else {
-        list += number
-        f1(number)
-      }
+  def findNumInList(number: Int, list: List[Int]): Boolean = {
+    list match {
+      case Nil => false
+      case head :: Nil => if (head == number) true else false
+      case head :: tail => if (head == number) true else findNumInList(number,tail)
     }
-    f1(number)
   }
 }
 
